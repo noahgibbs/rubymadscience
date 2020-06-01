447c3a5d1ba6d33d10048effd696d0c943f7055d
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-01 10:31:03 +0100

    Add topics controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'topics#index'
+  get 'topics/index'
+  get 'topics/show'
+  devise_for :users, path: :auth
 end
