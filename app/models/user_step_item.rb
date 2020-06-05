a43efb047f0208275d424c60891016f95c45db3f
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-05 11:09:17 +0100

    Add UserStepItems and display done-ness on the show view

--- app/models/user_step_item.rb
+++ app/models/user_step_item.rb
@@ -1,4 +1,22 @@
 class UserStepItem < ApplicationRecord
     belongs_to :user
     belongs_to :step
+
+    DONE_MAP = {
+        0 => "Not Done",
+        1 => "Skip",
+        2 => "Done",
+    }
+
+    def self.done_values
+        DONE_MAP.values
+    end
+
+    def self.done_as_text(int_value)
+        DONE_MAP[int_value]
+    end
+
+    def done_as_text
+        UserStepItem.done_as_text(self.doneness)
+    end
 end
