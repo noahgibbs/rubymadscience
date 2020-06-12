58cfd63c31c03890b81d6320d91bf6a97c0c04eb
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-12 17:29:19 +0100

    Add 'mark done' button to steps, coordinate with selector

--- app/javascript/packs/steps.js
+++ app/javascript/packs/steps.js
@@ -1,27 +1,50 @@
-$("body").on("change", "select.done-select", function() {
-    var changed_elt = $(this);
-    var step_id = changed_elt.data("step-id");
-    var set_value = this.value;
+var row_update_handler = function(row_jq_object, step_id, set_value) {
+    var row_elt = row_jq_object;
+    var button_elt = row_elt.find(".mark-done-button");
+    var selector_elt = row_elt.find("select.done-select");
 
-    // Disable the selector until the request finishes
-    changed_elt.prop('disabled', true);
+    button_elt.prop("disabled", true);
+    selector_elt.prop("disabled", true);
 
     $.ajax("/steps/update_done", {
         method: 'post',
         data: {
             step_id: step_id,
-            done: set_value,
-            note: "..."
+            done: 2,
+            note: ""
         },
         success: function() {
             $("#select-error-box-" + step_id).text("");
-            changed_elt.prop('disabled', false);
-            changed_elt.parents(".step-row").attr("data-server-value", set_value);
+            selector_elt.prop('disabled', false);
+            button_elt.prop('disabled', set_value != 0)
+            row_elt.attr("data-server-value", set_value);
+            selector_elt[0].value = set_value;
         },
         error: function() {
             $("#select-error-box-" + step_id).text("(Server error)");
-            changed_elt.prop('disabled', false);
-            changed_elt.parents(".step-row").attr("data-server-value", "error");
+            selector_elt.prop('disabled', false);
+            row_elt.attr("data-server-value", "error");
         }
     });
+}
+
+$("body").on("change", "select.done-select", function(e) {
+    var changed_elt = $(this);
+    var step_id = changed_elt.data("step-id");
+    var set_value = this.value;
+    var row_obj = changed_elt.parents(".step-row");
+
+    row_update_handler(row_obj, step_id, set_value);
+
+    e.preventDefault();
+});
+
+$("body").on("click", ".mark-done-button", function (e) {
+    var button_elt = $(this);
+    var step_id = button_elt.data("step-id");
+    var row_obj = button_elt.parents(".step-row");
+
+    row_update_handler(row_obj, step_id, 2); // set_value is 2, or "Done"
+
+    e.preventDefault();
 });
