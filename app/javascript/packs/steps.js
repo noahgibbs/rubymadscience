9abcd3b0b0a0b829db1fefab315e5962cac59a92
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-12 14:56:03 +0100

    Change step background colors when changing the selector for doneness

--- app/javascript/packs/steps.js
+++ app/javascript/packs/steps.js
@@ -1,6 +1,7 @@
 $("body").on("change", "select.done-select", function() {
     var changed_elt = $(this);
     var step_id = changed_elt.data("step-id");
+    var set_value = this.value;
 
     // Disable the selector until the request finishes
     changed_elt.prop('disabled', true);
@@ -9,16 +10,18 @@ $("body").on("change", "select.done-select", function() {
         method: 'post',
         data: {
             step_id: step_id,
-            done: this.value,
+            done: set_value,
             note: "..."
         },
         success: function() {
             $("#select-error-box-" + step_id).text("");
             changed_elt.prop('disabled', false);
+            changed_elt.parents(".step-row").attr("data-server-value", set_value);
         },
         error: function() {
             $("#select-error-box-" + step_id).text("(Server error)");
             changed_elt.prop('disabled', false);
+            changed_elt.parents(".step-row").attr("data-server-value", "error");
         }
     });
 });
