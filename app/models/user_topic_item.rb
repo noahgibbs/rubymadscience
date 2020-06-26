fb42c13b9a5b1d254b63e3795973dfd41926dc93
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-26 23:55:15 +0100

    Add UserTopicItem model

---
+++ app/models/user_topic_item.rb
@@ -0,0 +1,10 @@
+class UserTopicItem < ApplicationRecord
+  SUBSCRIPTION_VALUES = {
+    "none" => "None",
+    "daily" => "Daily",
+    "weekly" => "Weekly",
+    "monthly" => "Monthly",
+  }
+
+
+end
