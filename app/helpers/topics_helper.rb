14c4f20115a7f8b8af0a9539ed07dfa86204994e
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-26 23:57:09 +0100

    Slightly better markup for bootstrap button group

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,16 +1,16 @@
 module TopicsHelper
-    def bootstrap_button_group(options, selected:nil)
+    def bootstrap_button_group(options, selected:nil, top_classes: "")
         all_options = options.map { |value, name|
             <<HTML
 <label class="btn btn-primary#{ value == selected ? " active" : "" }">
-    <input type="radio" data-button-value="<%= value %>" autocomplete="off"#{ value == selected ? " checked" : "" } />
+    <input type="radio" data-button-value="#{ value }" autocomplete="off"#{ value == selected ? " checked" : "" } />
     #{name}
 </label>
 HTML
         }.join("\n")
 
         h = <<HTML
-<div class="btn-group btn-group-toggle" data-toggle="buttons">
+<div class="btn-group btn-group-toggle #{top_classes}" data-toggle="buttons">
 #{all_options}
 </div>
 HTML
