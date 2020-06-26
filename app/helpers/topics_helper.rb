078191b14a65317b4340851a3974e818b89d155b
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-06-26 22:31:53 +0100

    Prettier code for nonfunctional reminder mockup

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,19 @@
 module TopicsHelper
+    def bootstrap_button_group(options, selected:nil)
+        all_options = options.map { |value, name|
+            <<HTML
+<label class="btn btn-primary#{ value == selected ? " active" : "" }">
+    <input type="radio" data-button-value="<%= value %>" autocomplete="off"#{ value == selected ? " checked" : "" } />
+    #{name}
+</label>
+HTML
+        }.join("\n")
+
+        h = <<HTML
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+#{all_options}
+</div>
+HTML
+        h.html_safe
+    end
 end
