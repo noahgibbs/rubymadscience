1767d1aa178a8f2038052f3e0dbe75781e22a594
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-07-01 15:24:19 +0100

    More SendGrid config - seems to work now, at least initially

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,12 +31,26 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Do we care if the mailer can't send?
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # For now, use a real mailer. This may need to change later.
+  # Sendgrid config based on: https://sendgrid.com/docs/for-developers/sending-email/rubyonrails/
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "apikey",
+    password: Rails.application.credentials.sendgrid[:api_key],
+    domain: "rubymadscience.com",
+    address: "smtp.sendgrid.net",
+    port: 465,
+    authentication: :plain,
+    enable_starttls_auto: true,
+    tls: true,
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
