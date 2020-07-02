4b5a0d5aa6488154c94fee397a34d09a36dc56bf
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-07-02 10:17:52 +0100

    JS debugging for changing subscription from profile view

--- app/javascript/packs/steps.js
+++ app/javascript/packs/steps.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let row_update_handler = function(row_jq_object, step_id, set_value) {
     let row_elt = row_jq_object;
     let done_button_elt = row_elt.find(".mark-doneness-button[data-set-value=2]");
@@ -9,6 +11,7 @@ let row_update_handler = function(row_jq_object, step_id, set_value) {
     skip_button_elt.prop("disabled", true);
     undone_button_elt.prop("disabled", true);
 
+    console.log("Updating step " + step_id + " to have done-ness value " + set_value);
     $.ajax("/steps/update_done", {
         method: 'post',
         data: {
@@ -17,6 +20,7 @@ let row_update_handler = function(row_jq_object, step_id, set_value) {
             note: ""
         },
         success: function() {
+            console.log("Succeeded!");
             error_elt.text("");
             done_button_elt.prop('disabled', set_value != 0)
             skip_button_elt.prop('disabled', set_value != 0)
@@ -24,6 +28,7 @@ let row_update_handler = function(row_jq_object, step_id, set_value) {
             row_elt.attr("data-server-value", set_value);
         },
         error: function() {
+            console.log("Failed!");
             error_elt.text("(Server error)");
             row_elt.attr("data-server-value", "error");
         }
@@ -41,12 +46,13 @@ $("body").on("click", ".mark-doneness-button", function (e) {
     e.preventDefault();
 });
 
-$("body").on("click", ".topic-email-reminder-box .btn-group-toggle input", function (e) {
-    let new_value = $(".topic-email-reminder-box label.active input").data("button-value");
-    let topic_id = $(".topic-email-reminder-box").data("topic-id");
+let reminder_update_handler = function(e, row_elt) {
+    let new_value = row_elt.find("label.active input").data("button-value");
+    let topic_id = row_elt.data("topic-id");
 
-    $(".btn-group-toggle input").attr("disabled", true);
+    row_elt.find(".btn-group-toggle input").attr("disabled", true);
 
+    console.log("Updating topic " + topic_id + " subscription to have value " + new_value);
     $.ajax("/topics/update_subscription", {
         method: 'post',
         data: {
@@ -54,13 +60,24 @@ $("body").on("click", ".topic-email-reminder-box .btn-group-toggle input", funct
             subscription: new_value
         },
         success: function() {
-            $(".btn-group-toggle input").attr("disabled", false);
-            $(".subscription-error").text("");
+            console.log("Succeeded!");
+            row_elt.find(".btn-group-toggle input").attr("disabled", false);
+            row_elt.find(".subscription-error").text("");
         },
         error: function() {
-            console.log("Error setting subscription...")
-            $(".btn-group-toggle input").attr("disabled", false);
-            $(".subscription-error").text("Error setting subscription!");
+            console.log("Error setting subscription...");
+            row_elt.find(".btn-group-toggle input").attr("disabled", false);
+            row_elt.find(".subscription-error").text("Error setting subscription!");
         }
     });
+};
+
+$("body").on("click", ".topic-email-reminder-box .btn-group-toggle input", function (e) {
+    let row_elt = $(".topic-email-reminder-box");
+    return reminder_update_handler(e, row_elt);
+});
+
+$("body").on("click", ".profile-topic-row .btn-group-toggle input", function (e) {
+    let row_elt = $(this).parents(".profile-topic-row");
+    return reminder_update_handler(e, row_elt);
 });
