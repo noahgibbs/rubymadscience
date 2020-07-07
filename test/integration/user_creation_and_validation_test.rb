c5f7786f96ffb17a563c93d2eb53b635c64b6901
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-07-07 23:21:40 +0100

    Test more of new user workflow

--- test/integration/user_creation_and_validation_test.rb
+++ test/integration/user_creation_and_validation_test.rb
@@ -22,5 +22,35 @@ class UserCreationAndValidationTest < ActionDispatch::IntegrationTest
         assert_response :success
     end
 
+    test "Can sign up as new user" do
+        get new_user_registration_url
+        assert_response :success
 
+        assert_equal 0, ActionMailer::Base.deliveries.size
+        post user_registration_url,
+            params: {
+                user: {
+                    email: "newperson@example.com",
+                    password: "newpassword",
+                    password_confirmation: "newpassword",
+                }
+            }
+
+        assert_equal 1, ActionMailer::Base.deliveries.size
+        new_message = ActionMailer::Base.deliveries[0]
+        assert_equal ["newperson@example.com"], new_message.to
+        assert new_message.body.to_s["Confirm my account"], "Message body doesn't contain the string 'Confirm my account'!"
+
+        assert_response :redirect
+        follow_redirect!
+        assert_response :success
+
+        new_user = User.where(email: "newperson@example.com").first
+        assert new_user, "New user was not created!"
+        get "/auth/confirmation", params: { confirmation_token: new_user.confirmation_token }
+
+        assert_response :redirect
+        follow_redirect!
+        assert_response :success
+    end
 end
