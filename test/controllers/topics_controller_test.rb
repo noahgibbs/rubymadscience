8a3c5d5939bedd0eb936c96df501a82f229f2398
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-07-07 22:32:15 +0100

    Add logged-in test of topics controller

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
+    get topics_index_url
+    assert_response :success
+  end
+
+  test "should get index while signed in" do
+    #@request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in users(:admin)
     get topics_index_url
     assert_response :success
   end
