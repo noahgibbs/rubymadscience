0d9d84327f399472d9bb78cb8b5a2d0300d0ee9f
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-07-27 14:07:38 +0100

    Make sure that all production Topic data passes basic tests

--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -4,4 +4,14 @@ class TopicTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    Topic.topic_root = File.join(Rails.root, "app/models/topics")
+  end
+
+  test "All production Topic JSON parses as Topics" do
+    Topic.all.each do |topic|
+        # Don't need to do anything - it'll have parsed or not by this point.
+    end
+  end
 end
