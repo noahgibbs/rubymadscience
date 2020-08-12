d98e8cd7a219100754072ec43c248686b2f46ede
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-08-12 11:44:12 +0100

    Add an event for user password reset

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: :auth, controllers: {
     confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
   }
 
   get 'admin/dashboard'
